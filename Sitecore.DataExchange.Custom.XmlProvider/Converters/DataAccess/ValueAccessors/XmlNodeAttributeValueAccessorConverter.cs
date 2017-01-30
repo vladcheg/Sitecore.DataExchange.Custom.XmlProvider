c7d832b4f28105ea1c57affe437cf78c576c65df
DK\vls <vls@sitecore.net>
2017-01-30 19:05:26 +0200

    Xml Value Accessors.

---
+++ Sitecore.DataExchange.Custom.XmlProvider/Converters/DataAccess/ValueAccessors/XmlNodeAttributeValueAccessorConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace Sitecore.DataExchange.Custom.XmlProvider.Converters.DataAccess.ValueAccessors
+{
+    using Attributes;
+    using DataExchange.Converters.DataAccess.ValueAccessors;
+    using DataExchange.DataAccess;
+    using Repositories;
+    using Services.Core.Model;
+    using XmlProvider.DataAccess.Readers;
+
+    [SupportedIds("{69738136-E00F-4764-A07B-C875F3DB7280}")]
+    public class XmlNodeAttributeValueAccessorConverter : ValueAccessorConverter
+    {
+        public XmlNodeAttributeValueAccessorConverter(IItemModelRepository repository) : base(repository)
+        {
+        }
+
+        public override IValueAccessor Convert(ItemModel source)
+        {
+            var accessor = base.Convert(source);
+            if (accessor == null)
+            {
+                return null;
+            }
+
+            var attributeName = base.GetStringValue(source, "AttributeName");
+            if (string.IsNullOrEmpty(attributeName))
+            {
+                return null;
+            }
+
+            if (accessor.ValueReader == null)
+            {
+                accessor.ValueReader = new XmlNodeAttributeValueReader(attributeName);
+            }
+
+            return accessor;
+        }
+    }
+}
---
+++ Sitecore.DataExchange.Custom.XmlProvider/Converters/DataAccess/ValueAccessors/XmlNodeValueAccessorConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace Sitecore.DataExchange.Custom.XmlProvider.Converters.DataAccess.ValueAccessors
+{
+    using Attributes;
+    using DataExchange.Converters.DataAccess.ValueAccessors;
+    using DataExchange.DataAccess;
+    using Repositories;
+    using Services.Core.Model;
+    using XmlProvider.DataAccess.Readers;
+
+    [SupportedIds("")]
+    public class XmlNodeValueAccessorConverter : ValueAccessorConverter
+    {
+        public XmlNodeValueAccessorConverter(IItemModelRepository repository) : base(repository)
+        {
+        }
+
+        public override IValueAccessor Convert(ItemModel source)
+        {
+            var accessor = base.Convert(source);
+            if (accessor == null)
+            {
+                return null;
+            }
+
+            if (accessor.ValueReader == null)
+            {
+                accessor.ValueReader = new XmlNodeValueReader();
+            }
+
+            return accessor;
+        }
+    }
+}
