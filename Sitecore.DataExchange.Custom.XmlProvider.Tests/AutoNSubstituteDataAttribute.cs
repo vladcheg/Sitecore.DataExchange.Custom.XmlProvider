923e6bac8eb927618bcff01214b105c4d8ad8a60
DK\vls <vls@sitecore.net>
2017-01-31 11:19:10 +0200

    Add Test Project

---
+++ Sitecore.DataExchange.Custom.XmlProvider.Tests/AutoNSubstituteDataAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Sitecore.DataExchange.Custom.XmlProvider.Tests
+{
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoNSubstitute;
+    using Ploeh.AutoFixture.Xunit2;
+
+    public class AutoNSubstituteDataAttribute : AutoDataAttribute
+    {
+        public AutoNSubstituteDataAttribute()
+            : base(new Fixture()
+            .Customize(new AutoNSubstituteCustomization()))
+        {
+        }
+    }
+}
---
+++ Sitecore.DataExchange.Custom.XmlProvider.Tests/DataAccess/Readers/XmlNodeValueReaderTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.DataExchange.Custom.XmlProvider.Tests.DataAccess.Readers
+{
+    using System.Xml;
+    using DataExchange.DataAccess;
+    using FluentAssertions;
+    using Ploeh.AutoFixture.Xunit2;
+    using XmlProvider.DataAccess.Readers;
+    using Xunit;
+
+    public class XmlNodeValueReaderTests
+    {
+        [Theory, AutoData]
+        public void CanReadResult_NegativeResult(XmlNodeAttributeValueReader reader, int source, DataAccessContext context)
+        {
+            var canReadResult = reader.CanRead(source, context);
+
+            canReadResult.Should().NotBeNull();
+            canReadResult.CanReadValue.Should().BeFalse("because source is not {0}", typeof(XmlNode));
+        }
+    }
+}
